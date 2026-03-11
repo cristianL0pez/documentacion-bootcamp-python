aca61b3c1d90f7c2956d5ef686dfdb87c1bb13e6
gambito700 <gambitocerati@gmail.com>
2026-03-11 06:32:03 -0300

    docs: elimino borrar.txt de control-de-flujo y no de Get ChildItem  Recurse Filter

--- control-de-flujo/loops.py
+++ control-de-flujo/loops.py
@@ -1,11 +1,17 @@
 #primero los "for" para probar ejemplos de loops
 
 for i in range(5):
-"""
-for → es la instrucción que inicia el loop
-i → es el nombre de la variable, como lo nombramos, tienes razón
-in → significa "dentro de". Le dice al for dónde tiene que buscar los elementos. Se lee literalmente: "para cada i dentro de range(5)"
-range(5) → correcto, genera la secuencia de números del 0 al 4
-(5) → correcto, es lo que le indica hasta dónde llegar
-"""
-    print(i)
+# for es el programa
+# i es como lo nombramos
+# in que es ?
+# range es el programa que esta para revisar numero por numero de izquierda a derecha empezando por el 0 
+# (x) lo que este dentro es lo que revisara en ese orden
+    print(i, end=" " )
+# print(i, end=" ")   separa con espacio
+# print(i, end="-")   separa con guión
+# print(i, end=",")   separa con coma
+# print(i, end="")    todo junto
+# print(i, end="\n")  comportamiento por defecto
+
+for tree in range()
+# aqui quiero nombrar a una funcion "tree" que referira al 
